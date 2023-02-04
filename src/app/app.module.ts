ccf16c01d87fa15e83ba14c08092df4d7e0e8c97
Luis Henrique <luishenriquealbino@gmail.com>
2023-02-04 13:28:18 -0300

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PFTextfieldComponent } from './pftextfield/pftextfield.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PFTextfieldComponent,
   ],
   imports: [
     BrowserModule,
